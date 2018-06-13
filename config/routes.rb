ac868db22af71f4db9f2ca4fc5f39be82ac631da
Omar Ihmoda <omar.ihmoda@gmail.com>
2018-06-12 17:57:21 -0700

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get "foo/bar"
 
-  get 'static_pages/help'
+  get "foo/baz"
 
-  root "application#hello"
+  root "static_pages#home"
+
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/test"
+
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
 end
