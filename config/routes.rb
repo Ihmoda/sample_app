273637724a95eb5c722a9ecf6985af86affe092e
Omar Ihmoda <omar.ihmoda@gmail.com>
2018-06-13 10:18:59 -0700

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
+
   get "foo/bar"
 
   get "foo/baz"
 
   root "static_pages#home"
 
-  get "static_pages/home"
+  get "/help", to: "static_pages#help"
 
-  get "static_pages/help"
+  get "/about", to: "static_pages#about"
 
-  get "static_pages/test"
-
-  get "static_pages/about"
-
-  get "static_pages/contact"
+  get "/contact", to: "static_pages#contact"
 
 end
